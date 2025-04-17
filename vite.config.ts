3b1ea9fddd272c62a45106be57c14a9a4e7752a9
Maxine <mwandamaxyine@gmail.com>
2025-04-17 09:13:30 +0300

    specifying port

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, allowedHosts: ["whats-new-cx9e.onrender.com"],
+    host: true,
     strictPort: true,
     port: 9000,
     }, 
